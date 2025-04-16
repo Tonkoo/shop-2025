78034bc046449e6a0e0fc5237255edf8b360f731
Tonkoo <andrey54374@gmail.com>
2025-04-16 18:48:31 +0300

    feat: загрузил иконик для элемента radio. Начал верстку блока Сортировка

--- src/nuxt/nuxt.config.ts
+++ src/nuxt/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   // svgIconSprite: {
   //   sprites: {
   //     default: {
-  //       importPatterns: ['./client/modules/commonUI/assets/icon/default.svg'],
+  //       importPatterns: ['./client/modules/commonUI/assets/icon/radioFalseIcon.svg'],
   //     },
   //   },
   // },
